872adde99bbe5befefcf0f1f9361c2fafa56b0f1
benmelz <ben@melz.me>
2022-12-09 23:33:11 -0500

    feat: style with bootstrap

--- src/index.js
+++ src/index.js
@@ -38,7 +38,6 @@ function parseISOLocal(string) {
   if (!(string.match(/^\d{4}-\d{2}-\d{2}$/)))
     return null;
   const [year, month, day] = string.split('-').map((n) => parseInt(n));
-  console.log(`${year}, ${month}, ${day}`);
   return new Date(year, month - 1, day);
 }
 
@@ -78,7 +77,7 @@ function formatTimesheet(name, date) {
 ${name}
 
 ${rows}
-  `;
+  `.trim();
 }
 
 document.addEventListener('DOMContentLoaded', () => {
